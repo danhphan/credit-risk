f0a39d6afb7dda679aaa4895e697996cb5546e45
danhphan <danh.phan.mq@gmail.com>
2023-02-12 17:50:00 +1100

    Upload app html

--- docs/app/app.js
+++ docs/app/app.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://cdn.holoviz.org/panel/0.14.2/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.2/dist/wheels/panel-0.14.2-py3-none-any.whl', 'pyodide-http==0.1.0', 'arviz', 'holoviews>=1.15.1', 'hvplot', 'matplotlib', 'nbs', 'numpy', 'nutpie', 'pandas', 'pymc', 'pytensor', 'xarray']
+  const env_spec = ['https://cdn.holoviz.org/panel/0.14.2/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.2/dist/wheels/panel-0.14.2-py3-none-any.whl', 'pyodide-http==0.1.0', 'holoviews>=1.15.1', 'hvplot', 'pandas']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
@@ -47,109 +47,53 @@ from panel.io.pyodide import init_doc, write_doc
 
 init_doc()
 
-import numpy as np
 import pandas as pd
-import pymc as pm
-import nutpie
-import arviz as az
-import xarray as xr
-import pytensor.tensor as at
-import matplotlib.pyplot as plt
-from nbs.util import build_XY
-
-
-
 import hvplot.pandas
-import hvplot.xarray
 import panel as pn
 pn.extension('tabulator')
 
-gp_samples = az.InferenceData.from_netcdf("./nbs/mogp.nc")
 
-df = pd.read_csv("./data/interest_rates.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/danhphan/credit-risk/main/data/interest_rates.csv")
 df["date"] = pd.to_datetime(df["date"])
-
-n_outputs = 3
-country_mapping = {"US":0, "UK":1, "AU":2} 
-M = 90
-x_new = np.linspace(0, M-1, M)[:, None]
-X_new, _, _ = build_XY([x_new for idx in range(n_outputs)])
-
-dates_idx = pd.DataFrame({"date":pd.date_range("2016-01-01", "2023-06-01", freq='MS')}).reset_index()
-dates_idx = dates_idx.rename(columns={"index":"x"})
-print(dates_idx.shape)
-dates_idx.head()
-
-
-pred_results = {}
-
-f_pred = gp_samples.posterior_predictive["preds"].sel(chain=0)
-
-for idx, country in enumerate(country_mapping.keys()):
-    # Prediction
-    print(idx, country)
-    y_ = f_pred[:,M*idx:M*(idx+1)]
-    y_["preds_dim_2"] = ("preds_dim_2", dates_idx["date"])
-    pred_results[country] = y_
-
 
 # Make DataFrame Pipeline Interactive
 idf = df.interactive()
 
-WIDTH = 600
+country_mapping = {"US":0, "UK":1, "AU":2} 
+pred_results = {}
+
+for idx, country in enumerate(country_mapping.keys()):
+    # Prediction
+    print(idx, country)
+    df_data = pd.read_csv(f"https://raw.githubusercontent.com/danhphan/credit-risk/main/data/prediction_{country}.csv")
+    df_data['Date'] = pd.to_datetime(df_data['Date'])
+    df_data = df_data.set_index("Date")
+    pred_results[country] = df_data
 
 
-chart_data = (idf.hvplot(x = 'date', by='country', y='rate', kind='scatter', xlabel="Date", ylabel="Observed interest rate", 
-                         line_width=2, title="Interest rates by Country", width=WIDTH) *
-              idf.hvplot(x = 'date', by='country', y='rate', line_width=1)
+WIDTH = 550
+chart_data = (idf.hvplot(x = 'date', by='country', y='rate', kind='scatter', 
+                         xlabel="Date", ylabel="Observed interest rate", width=WIDTH,
+                         line_width=2, title="Historical interest rates by Country") *
+              idf.hvplot(x = 'date', by='country', y='rate', line_width=1, color=["blue", "green", "red"])
               ).opts(legend_position='top', legend_offset=(150, 0))
 
 
 
-def plot(pred_results, country):
+# Make DataFrame Pipeline Interactive
+def plot(pred_results, country, color='red'):
     y_iteractivate = pred_results[country].interactive()
-    panel_plot = (y_iteractivate.quantile(q=0.1, dim="draw").hvplot(label="10% Quantile", alpha=0.5, 
-                        xlabel='Date', ylabel="Predicted interest rate", width=WIDTH, title=f'Prediction for {country}') * 
-                  y_iteractivate.quantile(q=0.9, dim="draw").hvplot(label="90% Quantile", alpha=0.5) *
-                  y_iteractivate.mean(dim="draw").hvplot(label='mean', color='red', line_width=3) )
+    panel_plot = (y_iteractivate.hvplot(label="10% Quantile", alpha=0.5, x="Date", y="Q10", width=WIDTH,
+                        xlabel='Date', ylabel="Predicted interest rate", title=f'Prediction for {country}') * 
+                  y_iteractivate.hvplot(label="90% Quantile", x="Date", y="Q90", alpha=0.3) *
+                  y_iteractivate.hvplot(label='mean', x="Date", y="Mean", color=color, line_width=3) )
     return panel_plot.opts(legend_position='top', legend_offset=(150,0))
 
 
-pred_au = plot(pred_results, 'AU')
-pred_us = plot(pred_results, 'US')
-pred_uk = plot(pred_results, 'UK')
 
-
-#Layout using Template
-template = pn.template.FastListTemplate(
-    title='Interest rates prediction for US, AU and UK', 
-    sidebar=[pn.pane.Markdown("# Interest rates prediction"), 
-             pn.pane.Markdown("#### Interest rates prediction for US, AU and UK. This project is used for technology demonstration purpose, and should not be used for the investment decision-making."), 
-#              pn.pane.PNG('climate_day.png', sizing_mode='scale_both'),
-             ],
-    main=[pn.Row(pn.Column(chart_data.panel(width=WIDTH), margin=(0,25)), 
-                 pred_au.panel(width=WIDTH)), 
-          pn.Row(pn.Column(pred_us.panel(width=WIDTH), margin=(0,25)), 
-                 pn.Column(pred_uk.panel(width=WIDTH)))],
-    accent_base_color="#88d8b0",
-    header_background="#88d8b0",
-)
-# template.show()
-template.servable();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+pred_au = plot(pred_results, 'AU', 'blue')
+pred_us = plot(pred_results, 'US', 'green')
+pred_uk = plot(pred_results, 'UK', 'red')
 
 #Layout using Template
 template = pn.template.FastListTemplate(
@@ -158,15 +102,17 @@ template = pn.template.FastListTemplate(
              pn.pane.Markdown("#### Interest rates prediction for US, AU and UK. This project is used for technology demonstration purpose, and should not be used for the investment decision-making."), 
 #              pn.pane.PNG('climate_day.png', sizing_mode='scale_both'),
              ],
-    main=[pn.Row(pn.Column(chart_data.panel(width=WIDTH), margin=(0,25)), 
-                 pred_au.panel(width=WIDTH)), 
-          pn.Row(pn.Column(pred_us.panel(width=WIDTH), margin=(0,25)), 
-                 pn.Column(pred_uk.panel(width=WIDTH)))],
+    main=[pn.Row(pn.Column(chart_data.panel()), 
+                 pred_au.panel()), 
+          pn.Row(pn.Column(pred_us.panel()), 
+                 pn.Column(pred_uk.panel()))],
     accent_base_color="#88d8b0",
     header_background="#88d8b0",
+    sizing_mode="stretch_both"
 )
-# template.show()
-template.servable();
+
+template.servable()
+
 
 await write_doc()
   `
